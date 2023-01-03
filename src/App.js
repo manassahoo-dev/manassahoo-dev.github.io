516b804302cb3e84d2b1444ed90240677b4f9e54
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-01-03 23:22:31 +0530

    Add Button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ function App() {
     <div className='App'>
       <h1>Hey, I'm Manas 👋</h1>
       <h3>Front End Developer based in Hyderabad, India</h3>
+
+      <button>React Js</button>
+      <button>Angular</button>
+      <button>JavaScript</button>
+      <button>TypeScript</button>
+      <button>Html</button>
+
       <footer>Manas Sahoo</footer>
     </div>
   );
