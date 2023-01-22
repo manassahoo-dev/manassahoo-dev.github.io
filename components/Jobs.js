02fc6381cc60e107838185a8879a55c000da064c
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-01-22 23:07:14 +0530

    Add more links

--- components/Jobs.js
+++ components/Jobs.js
@@ -15,6 +15,16 @@ export default function Jobs() {
       title: 'NoDesk - Where Everyone Works Remote',
       description:
         'Browse and apply to the best new remote jobs at leading remote companies and startups for free. Join hundreds of companies that use NoDesk to build their remote teams.'
+    },
+    {
+      link: 'https://remotive.com',
+      title: 'Remote Jobs in Programming, Support, Design and more',
+      description: `Find the best remote job, working as a developer, customer support rep, product or sales professional... See openings in our categories. All jobs are hand curated and allow remote work. We serve the best work from home jobs in popular categories. Talent is everywhere, work remotely today!`
+    },
+    {
+      link: 'https://www.flexjobs.com',
+      title: 'FlexJobs: The Best Remote Jobs, Work from Home Jobs & More',
+      description: `The #1 job site for hand-screened remote jobs (work from home jobs) since 2007. No ads, scams or junk. Discover a better online job search with FlexJobs!`
     }
   ]
   return (
