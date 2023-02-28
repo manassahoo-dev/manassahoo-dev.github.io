c7b3c3c5dc28ae57e517f71350706408e5a58d52
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-02-28 23:54:33 +0530

    Update Hero Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,16 @@ export default function Home() {
         <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
           <div className='mr-auto place-self-center lg:col-span-7'>
             <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white'>
-              Payments tool for software companies
+              Welcome.
             </h1>
             <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+              My name is Manas Sahoo, I'm a front-end developer based in
+              Hyderabad, India. I have developed many types of front-ends from
+              well know DJ applications to Ecommerce booking platforms.
+            </p>
+            <p className='max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400'>
+              I'm passionate about cutting-edge, pixel-perfect, beautiful
+              interfaces and intuitively implemented UX.{' '}
             </p>
             <a
               href='#'
