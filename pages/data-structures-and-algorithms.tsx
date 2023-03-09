055838571afda0962b4b7fadbee3c447512d39bb
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-09 23:55:02 +0530

    Add Meta Tags

--- pages/data-structures-and-algorithms.tsx
+++ pages/data-structures-and-algorithms.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 function DSA() {
-  return <div>DSA</div>;
+  const list = ['Array', 'Linked List', 'Tree'];
+  return (
+    <div>
+      DSA
+      {list.map((item) => (
+        <>
+          <a
+            href='#'
+            className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'
+          >
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              {item}
+            </h5>
+            <p className='font-normal text-gray-700 dark:text-gray-400'>
+              Here are the biggest enterprise technology acquisitions of 2021 so
+            </p>
+          </a>
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default DSA;
