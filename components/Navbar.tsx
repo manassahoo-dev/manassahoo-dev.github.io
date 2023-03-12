1c361be1b9dd5d4c73ee97b62e1bedaefa041fff
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-12 23:24:37 +0530

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,50 +1,15 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
+    <nav>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
         <Link href='/' className='flex items-center'>
-          <img
-            src='https://flowbite.com/docs/images/logo.svg'
-            className='h-6 mr-3 sm:h-9'
-            alt='Flowbite Logo'
-          />
-          <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
-            Manas Sahoo
-          </span>
+          <Image src='/logo.png' alt='logo' width={128} height={48} />
         </Link>
-        <div className='flex md:order-2'>
-          <button
-            type='button'
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-          >
-            Get started
-          </button>
-          <button
-            data-collapse-toggle='navbar-cta'
-            type='button'
-            className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-            aria-controls='navbar-cta'
-            aria-expanded='false'
-          >
-            <span className='sr-only'>Open main menu</span>
-            <svg
-              className='w-6 h-6'
-              aria-hidden='true'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                fill-rule='evenodd'
-                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-                clip-rule='evenodd'
-              ></path>
-            </svg>
-          </button>
-        </div>
+
         <div
           className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
           id='navbar-cta'
@@ -59,26 +24,17 @@ export default function Navbar() {
               </Link>
             </li>
             <li>
-              <Link
-                href='/data-structures-and-algorithms'
-                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
+              <Link href='/data-structures-and-algorithms' className='link'>
                 DSA
               </Link>
             </li>
             <li>
-              <Link
-                href='/interview-questions-answers'
-                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
+              <Link href='/interview-questions-answers' className='link'>
                 Interview Question
               </Link>
             </li>
             <li>
-              <a
-                href='#'
-                className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-              >
+              <a href='#' className='link'>
                 Resume
               </a>
             </li>
