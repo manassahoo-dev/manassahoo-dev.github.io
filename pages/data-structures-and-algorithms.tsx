51710a82d1e8a1098a1d0cc0e206770683eecb20
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-16 23:56:35 +0530

    DSA Page

--- pages/data-structures-and-algorithms.tsx
+++ pages/data-structures-and-algorithms.tsx
@@ -13,21 +13,18 @@ function DSA() {
           key='desc'
         />
       </Head>
-      {list.map((item) => (
-        <>
+      <div className='flex gap-4 justify-between'>
+        {list.map((item) => (
           <a
             href='#'
-            className='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'
+            className='block max-w-sm p-4  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'
           >
-            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+            <h5 className='text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>
               {item}
             </h5>
-            <p className='font-normal text-gray-700 dark:text-gray-400'>
-              Here are the biggest enterprise technology acquisitions of 2021 so
-            </p>
           </a>
-        </>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
