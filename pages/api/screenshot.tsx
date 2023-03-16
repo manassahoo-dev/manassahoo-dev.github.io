3aef01267eefc9bde67ec895cddbd356ec42b314
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-17 01:05:47 +0530

    Add Screenshot API

--- pages/api/screenshot.tsx
+++ pages/api/screenshot.tsx
@@ -14,11 +14,21 @@ export default async function handler(
   let browser;
 
   try {
+    let domain = new URL(url);
     browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({ width: 1600, height: 1200 });
-    await page.goto(url);
-    const screenshot = await page.screenshot({ type: 'png' });
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const fileName = `public/screenshots/${domain.hostname}.png`;
+    const screenshot = await page.screenshot({
+      type: 'png',
+      path: fileName,
+    });
+    res.setHeader('Content-Type', 'image/png');
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename=${domain.hostname}.png`
+    );
     res.status(200).send(screenshot);
   } catch (error) {
     res.status(500).json({ error: (error as any).message });
