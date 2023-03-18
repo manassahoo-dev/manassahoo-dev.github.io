fd29e2aac353f62a86c4c1922ed48845131ccdd3
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-18 12:58:34 +0530

    Configure geist-ui/core in App.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { GeistProvider, CssBaseline } from '@geist-ui/core';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <GeistProvider>
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GeistProvider>
   );
 }
