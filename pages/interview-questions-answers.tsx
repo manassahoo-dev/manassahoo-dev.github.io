6ba9ee60e58f1a0ef0e3dcd402c800bf8707d21d
Manas Ranjan Sahoo <manas_sahoo@outlook.com>
2023-03-21 23:49:30 +0530

    Add DSA

--- pages/interview-questions-answers.tsx
+++ pages/interview-questions-answers.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function InterviewQnA() {
-  return <div>InterviewQnA</div>;
+  return (
+    <div>
+      <li>JavaScript</li>
+      <li>TypeScript</li>
+      <li>React.js</li>
+      <li>Angular</li>
+      <li>Html</li>
+    </div>
+  );
 }
 
 export default InterviewQnA;
